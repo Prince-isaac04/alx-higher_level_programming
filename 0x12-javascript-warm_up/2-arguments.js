5bb8c9d2b81dd86d44cd0ca1bc84152ad506012b
Prince-isaac04 <ajayioluwaseyi1692002@gmail.com>
2024-05-19 22:44:26 +0000

    pyton warm uo

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,15 +1,13 @@
 #!/usr/bin/node
 const process = require('process');
 let message;
-if(process.argv.lenght === 3){
-	message = "argument found";
+
+if (process.argv.length === 3) {
+  message = 'Argument found';
+} else if (process.argv.length < 3) {
+  message = 'No argument';
+} else {
+  message = 'Arguments found';
 }
-else if(process.argv.lenght < 3){
-	message = "no argument";
-}
-else{
-message = "argument found"
-}
+
 console.log(message);
-
-
